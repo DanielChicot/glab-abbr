5eece4bdf560853c3ec400c8e5350fc7e9c9bbcb
Daniel Chicot <daniel.chicot@dlapiper.com>
2024-08-07 19:30:17 +0100

    More abbreviations.

--- conf.d/glab-abbr.fish
+++ conf.d/glab-abbr.fish
@@ -1,4 +1,17 @@
 abbr -a -- gl glab
-abbr -a -- glm 'glab mr'
+
 abbr -a -- glc 'glab ci'
+abbr -a -- glcl 'glab ci list'
+abbr -a -- glcv 'glab ci view'
+
+abbr -a -- glm 'glab mr'
+abbr -a -- glmc 'glab mr create'
+abbr -a -- glmm 'glab mr merge'
+abbr -a -- glmu 'glab mr update'
+
 abbr -a -- glv 'glab variable'
+abbr -a -- glvg 'glab variable get'
+abbr -a -- glvgs 'glab variable get --scope'
+abbr -a -- glvl 'glab variable list'
+abbr -a -- glvs 'glab variable set'
+abbr -a -- glvss 'glab variable set --scope'
