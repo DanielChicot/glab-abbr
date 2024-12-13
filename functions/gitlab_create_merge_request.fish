8aa5f88689c23d05886d7bb357e45c7e70534660
Daniel Chicot <daniel.chicot@dlapiper.com>
2024-12-13 21:04:06 +0000

    Added gitlab_create_merge_request, and a few abbreviations.

---
+++ functions/gitlab_create_merge_request.fish
@@ -0,0 +1,32 @@
+function gitlab_create_merge_request -a ticket description
+
+
+    argparse w/worktree -- $argv
+    or return
+
+    if test (count $argv) -ne 2
+        echo Usage: (status function) [-w] ticket description >&2
+        return 1
+    end
+
+    glab mr create \
+        --assignee Daniel.Chicot \
+        --source-branch $argv[1] \
+        --create-source-branch \
+        --target-branch (git_main_branch) \
+        --squash-before-merge \
+        --draft \
+        --remove-source-branch \
+        --title "$argv[1]: $argv[2]" \
+        --yes
+
+    if test $status -eq 0
+        git fetch --all --prune
+    end
+
+    if test $status -eq 0
+        if set -q _flag_w
+            git worktree add --guess-remote ./worktrees/$argv[1] $argv[1]
+        end
+    end
+end
